27acf4579dfba01e68655bc50ca0a7cc4e6c7a71
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-11-27 19:03:42 +0100

    fixed App.js and started the log in page

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,24 +1,9 @@
-import logo from './Images/Netflix.png';
-import './css/App.css';
+
+import LoginPage from './LogInPage.js';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Netflix
-        </a>
-      </header>
-    </div>
-  );
+  return (<LoginPage />);
 }
 
 export default App;
--- netflix/src/LogInPage.js
+++ netflix/src/LogInPage.js
@@ -1,6 +1,28 @@
-function LogIn(){
-    return(
-    <a>baaaaaa</a>
+import React from 'react';
 
+export default function LogIn(){
+    return (
+        <div className="loginContainer">
+            <form className="loginForm" onSubmit="#">
+                <label>Email address:</label>
+                <div className="loginInputContainer">
+                    <img src="#" alt="" className="inputIcon" />
+                    <input 
+                        type="email" 
+                        
+                        placeholder='Input Email'
+                    />
+                </div>
+                <label>Password:</label>
+                <div className="loginInputContainer">
+                    
+                    <input
+                        type="password" placeholder='Input Password'/>
+                </div>
+                {/* <Link to="/register" className="registerLink">New user? <strong>Sign Up</strong></Link> */}
+                <input type="submit" value="Log In" className="loginButton" name='LogInButton'/>
+                {/* {error && <div className="authError">{  }</div>} */}
+            </form>
+        </div>
     );
 }
