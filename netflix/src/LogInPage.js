b7156257c730b7cd307e828eaa2636dbc2f2e029
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-11-30 22:22:05 +0100

    code cleanup and json and pakage update

--- netflix/src/LogInPage.js
+++ netflix/src/LogInPage.js
@@ -3,27 +3,20 @@ import './css/LogInPage.css';
 
 export default function LogIn(){
     return (
+    <div id='mainGrid'>
         <div className="loginContainer">
             <form className="loginForm" onSubmit="#">
                 <label>Email address:</label>
-                <div className="loginInputContainer">
-                    <img src="#" alt="" className="inputIcon" />
-                    <input 
-                        type="email" 
-                        
-                        placeholder='Input Email'
-                    />
-                </div>
+                <input type="email" placeholder='Input Email'/>
+              
                 <label>Password:</label>
-                <div className="loginInputContainer">
-                    
-                    <input
-                        type="password" placeholder='Input Password'/>
-                </div>
+                <input type="password" placeholder='Input Password'/>
+                
                 {/* <Link to="/register" className="registerLink">New user? <strong>Sign Up</strong></Link> */}
                 <input type="submit" value="Log In" className="loginButton" name='LogInButton'/>
                 {/* {error && <div className="authError">{  }</div>} */}
             </form>
         </div>
+    </div>
     );
 }
