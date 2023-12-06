582c4625d8cd9c002a4ddeff009ec6fb192d264b
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-12-06 17:38:49 +0100

    updated the code file to have a proper versin of a js form and variabels

--- netflix/src/LogInPage.js
+++ netflix/src/LogInPage.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/LogInPage.css';
 
-export default function LogIn(){
-    return (
+export default function LogIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Add your authentication logic here
+    // For simplicity, I'm just checking if both email and password are not empty
+    if (email && password) {
+      // Perform authentication or API request here
+      // If successful, you can navigate to the main content page
+      console.log('Login successful');
+    } else {
+      setError('Please enter both email and password');
+    }
+  };
+
+  return (
     <div id='mainGrid'>
-        <div className="loginContainer">
-            <form className="loginForm" onSubmit="#">
-                <label>Email address:</label>
-                <input type="email" placeholder='Input Email'/>
-              
-                <label>Password:</label>
-                <input type="password" placeholder='Input Password'/>
-                
-                {/* <Link to="/register" className="registerLink">New user? <strong>Sign Up</strong></Link> */}
-                <input type="submit" value="Log In" className="loginButton" name='LogInButton'/>
-                {/* {error && <div className="authError">{  }</div>} */}
-            </form>
-        </div>
+      <div className="loginContainer">
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <label>Email address:</label>
+          <input
+            type="email"
+            placeholder='Input Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label>Password:</label>
+          <input
+            type="password"
+            placeholder='Input Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {error && <div className="authError">{error}</div>}
+
+          {/* Uncomment the following lines if you have a registration page */}
+          {/* <Link to="/register" className="registerLink">
+            New user? <strong>Sign Up</strong>
+          </Link> */}
+
+          <input
+            type="submit"
+            value="Log In"
+            className="loginButton"
+            name='LogInButton'
+          />
+        </form>
+      </div>
     </div>
-    );
+  );
 }
