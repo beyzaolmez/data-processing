e26916f067f7769925c611ca415b21380847655b
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-12-11 13:47:49 +0100

    front end update and welcome message

--- netflix/src/LogInPage.js
+++ netflix/src/LogInPage.js
@@ -6,6 +6,7 @@ export default function LogIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,7 +16,8 @@ export default function LogIn() {
     if (email && password) {
       // Perform authentication or API request here
       // If successful, you can navigate to the main content page
-      console.log('Login successful');
+      // For demonstration purposes, I'm just setting isLoggedIn to true
+      setIsLoggedIn(true);
     } else {
       setError('Please enter both email and password');
     }
@@ -23,43 +25,46 @@ export default function LogIn() {
 
   return (
     <div id='mainGrid'>
-      <img src={LogoImage} alt="Netflix" id="logoImage"/>
+      <img src={LogoImage} alt="Netflix" id="logoImage" />
       <div className="loginContainer">
-        <form className="loginForm" onSubmit={handleSubmit}>
-          <label for='userEmail'>Email address:</label>
-          <input
-            type="email"
-            id='userEmail'
-            name='userEmail'
-            placeholder='Input Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        {isLoggedIn ? (
+          <div className="loggedInInfo">
+            <p>Welcome, {email}!</p>
+            {/* Add additional information or navigation for logged-in users */}
+          </div>
+        ) : (
+          <form className="loginForm" onSubmit={handleSubmit}>
+            <label htmlFor='userEmail'>Email address:</label>
+            <input
+              type="email"
+              id='userEmail'
+              name='userEmail'
+              placeholder='Input Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-          <label for="">Password:</label>
-          <input
-            type="password"
-            id="userPassword"
-            name="userPassword"
-            placeholder='Input Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            <label htmlFor="userPassword">Password:</label>
+            <input
+              type="password"
+              id="userPassword"
+              name="userPassword"
+              placeholder='Input Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-          {error && <div className="authError">{error}</div>}
+            {error && <div className="authError">{error}</div>}
 
-          {/* Uncomment the following lines if you have a registration page */}
-          {/* <Link to="/register" className="registerLink">
-            New user? <strong>Sign Up</strong>
-          </Link> */}
-          <a id="FormLink" href="ForgotPasswordPage.js">Forgot password?</a>
-          <input
-            type="submit"
-            value="Log In"
-            className="loginButton"
-            name='LogInButton'
-          />
-        </form>
+            <a id="FormLink" href="ForgotPasswordPage.js">Forgot password?</a>
+            <input
+              type="submit"
+              value="Log-In"
+              className="loginButton"
+              name='LogInButton'
+            />
+          </form>
+        )}
       </div>
     </div>
   );
