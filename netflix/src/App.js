6a86ed7c353e5efa878292f86c5f08a0120a1872
CadarIustin <iustin.cadar@student.nhlstenden.com>
2024-01-16 17:23:09 +0100

    Fixed the series bug

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -15,10 +15,15 @@ function App() {
     };
 
     const handleSeriesUpload = (event) => {
-      const file = event.target.files[0];
-      const videoUrl = URL.createObjectURL(file);
-      setSeries([...series, { episode: series.length + 1, url: videoUrl }]);
+      const files = event.target.files;
+      const newSeries = Array.from(files).map((file, index) => ({
+          episode: series.length + index + 1,
+          url: URL.createObjectURL(file)
+      }));
+  
+      setSeries(series.concat(newSeries));
   };
+  
 
   const handleDelete = () => {
     window.location.reload();
@@ -41,23 +46,33 @@ function App() {
     window.location.reload();
 };
 
-
 return (
   <div className="admin-container">
       <AddItemForm />
       <ItemList />
       <form onSubmit={handleSubmit}>
-          {}
-          <input type="file" accept="video/*" onChange={handleVideoUpload} style={{ display: 'none' }} id="video-upload" />
-          <label htmlFor="video-upload" className="upload-button">Upload Video</label>
-          {videoFile && <VideoControls videoSrc={videoFile} />}
+                {/* Conditionally render "Upload Video" button */}
+                {!videoFile && series.length === 0 && (
+                    <>
+                        <input type="file" accept="video/*" onChange={handleVideoUpload} style={{ display: 'none' }} id="video-upload" />
+                        <label htmlFor="video-upload" className="upload-button">Upload Video</label>
+                    </>
+                )}
+                {videoFile && <VideoControls videoSrc={videoFile} />}
 
-          {}
-          <input type="file" accept="video/*" onChange={handleSeriesUpload} style={{ display: 'none' }} id="series-upload" multiple />
-          <label htmlFor="series-upload" className="upload-button">Upload Series</label>
-          {}
+                {/* Render "Upload Series" button only if no series videos are added */}
+                {!videoFile && series.length === 0 && (
+                    <>
+                        <input type="file" accept="video/*" onChange={handleSeriesUpload} style={{ display: 'none' }} id="series-upload" multiple />
+                        <label htmlFor="series-upload" className="upload-button">Upload Series</label>
+                    </>
+                )}
 
-          {}
+                {/* Display all series videos */}
+                {series.map((video, index) => (
+                    <VideoControls key={index} videoSrc={video.url} />
+                ))}
+
           <input 
               type="submit" 
               value="Submit" 
