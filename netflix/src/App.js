618ae31bfca991627af345e7dd56bbd5ee85af7c
CadarIustin <iustin.cadar@student.nhlstenden.com>
2024-01-16 17:47:27 +0100

    Added genres to the movies/series

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -7,6 +7,7 @@ import VideoControls from './AdminPanel/VideoControls';
 function App() {
     const [videoFile, setVideoFile] = useState(null);
     const [series, setSeries] = useState([]);
+    const [genres, setGenres] = useState({ action: false, comedy: false, drama: false });
 
     const handleVideoUpload = (event) => {
         const file = event.target.files[0];
@@ -23,8 +24,14 @@ function App() {
   
       setSeries(series.concat(newSeries));
   };
-  
+   
+  const handleGenreChange = (event) => {
+    setGenres({ ...genres, [event.target.name]: event.target.checked });
+};
 
+const isAnyGenreSelected = Object.values(genres).some(Boolean);
+const isSubmitEnabled = (videoFile || series.length > 0) && isAnyGenreSelected;
+  
   const handleDelete = () => {
     window.location.reload();
 };
@@ -51,16 +58,16 @@ return (
       <AddItemForm />
       <ItemList />
       <form onSubmit={handleSubmit}>
-                {/* Conditionally render "Upload Video" button */}
+                {}
                 {!videoFile && series.length === 0 && (
                     <>
                         <input type="file" accept="video/*" onChange={handleVideoUpload} style={{ display: 'none' }} id="video-upload" />
-                        <label htmlFor="video-upload" className="upload-button">Upload Video</label>
+                        <label htmlFor="video-upload" className="upload-button">Upload Movies</label>
                     </>
                 )}
                 {videoFile && <VideoControls videoSrc={videoFile} />}
 
-                {/* Render "Upload Series" button only if no series videos are added */}
+                {}
                 {!videoFile && series.length === 0 && (
                     <>
                         <input type="file" accept="video/*" onChange={handleSeriesUpload} style={{ display: 'none' }} id="series-upload" multiple />
@@ -68,16 +75,29 @@ return (
                     </>
                 )}
 
-                {/* Display all series videos */}
+                {}
                 {series.map((video, index) => (
                     <VideoControls key={index} videoSrc={video.url} />
                 ))}
+                 <h3>Choose genres:</h3>
+                <div>
+                    <input type="checkbox" id="action" name="action" checked={genres.action} onChange={handleGenreChange} />
+                    <label htmlFor="action">Action</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="comedy" name="comedy" checked={genres.comedy} onChange={handleGenreChange} />
+                    <label htmlFor="comedy">Comedy</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="drama" name="drama" checked={genres.drama} onChange={handleGenreChange} />
+                    <label htmlFor="drama">Drama</label>
+                </div>
 
           <input 
               type="submit" 
               value="Submit" 
               className="submit-button"
-              disabled={!videoFile && series.length === 0}
+              disabled={!isSubmitEnabled}
           />
       </form>
       <DeleteButton onClick={handleDelete} />
